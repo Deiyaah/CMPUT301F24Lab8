3129f8a420819eb80db00a75244e68b939249613
Deiyaah <diyaay30@gmail.com>
2024-10-23 17:47:54 -0600

    adding the has city test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -25,13 +26,25 @@ public class CustomListTest {
     /**
      * get the size of the list
      * increase the list by adding a new city
-     * check if our current size matches the initial size
-     plus one */
+     * check if our current size matches the initial size plus one
+     */
     @Test
     public void addCityTest(){
         list = MockCityList();
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    /**
+     * checks if the city belongs in the list
+     */
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+        City city = new City("Regina", "Saskatchewan");
+        list.add(city);
+        list.hasCity(city);
+        assertEquals("City belongs in the list", list.hasCity(city));
     }
 }
